a6c3ecae88fbd4e613d355a98e5e0ae050fd91d6
Patrick Barabe <pbarabe@arizona.edu>
2021-05-25 13:10:36 -0700

    Revert tests to known-good-state
    New tests work reliably when running locally, but fail
    reliably on GitHub. Starting over.
    
    references #4

--- test/index.js
+++ test/index.js
@@ -2,80 +2,42 @@
 'use strict'
 
 const AWS = require('aws-sdk')
-const s3 = new AWS.S3()
 const expect = require('chai').expect
 const fs = require('fs')
+const s3 = new AWS.S3()
 const S3Publisher = require('../index')
 
 let testPublisher = new S3Publisher({
   keyPrefix: 'testS3Publisher',
-  bucket: process.env.AWS_S3_BUCKET,
-  exclusions: ['.map']
+  bucket: process.env.AWS_S3_BUCKET
 })
-let testDir0 = './test/foo' // test dir
+let testDir0 = './test/foo' // test dir & files
 let testDir1 = './test/empty' // empty
 let testDir2 = './test/nil' // doesn't exist
 
-// make sure the remote path is clear
-const cleanRemote = () => {
-  s3.listObjectsV2(
-    {
-      Bucket: process.env.AWS_S3_BUCKET,
-      Prefix: 'testS3Publisher'
-    }, (err, data) => {
-      if (err) {
-        console.error(err)
-      }
-
-      if (data.Contents.length > 0) {
-        s3.deleteObjects({
-          Bucket: process.env.AWS_S3_BUCKET,
-          Delete: {
-            Objects: data.Contents.map(({ Key }) => ({ Key }))
-          }
-        }, (delErr, delData) => {
-          if (delErr) {
-            console.error(delErr)
-          }
-        })
-      }
-    }
-  )
-}
-
 describe('S3Publisher', () => {
   before((done) => {
-    // start with a clean remote path
-    cleanRemote()
-
-    // create an empty dir to test
     if (!fs.existsSync(testDir1)) {
       fs.mkdirSync(testDir1)
     }
-
     done()
   })
 
   after((done) => {
-    // cleanup remote path
-    cleanRemote()
-
-    // remove the empty dir
     if (fs.existsSync(testDir1)) {
       fs.rmdirSync(testDir1)
     }
-
     done()
   })
 
   it('Should error if not instantiated with a params object', (done) => {
-    expect(function () { new S3Publisher() }).to.throw(Error)
-    done()
+      expect(function () { new S3Publisher() }).to.throw(Error)
+      done()
   })
 
   it('Should error if not instantiated with a bucket parameter', (done) => {
-    expect(function () { new S3Publisher({}) }).to.throw(Error)
-    done()
+      expect(function () { new S3Publisher({}) }).to.throw(Error)
+      done()
   })
 
   it('It instantiates a valid object when when required parameters are passed to the constructor', (done) => {
@@ -83,11 +45,20 @@ describe('S3Publisher', () => {
     done()
   })
 
-  describe('S3Publisher.publish()', () => {
+  describe('publish', () => {
     it('Returns an error if the source directory does not exist', (done) => {
       testPublisher.publish(testDir2, (err, data) => {
         expect(err).to.be.an('Error')
         expect(data).to.be.undefined
+      })
+      done()
+    })
+
+    it('Uploads all files in a directory', (done) => {
+      testPublisher.publish(testDir0, (err, data) => {
+        expect(data).to.be.an('object')
+        expect(data).to.have.propery('ETag')
+        expect(data).to.have.propery('s3file')
       })
       done()
     })
@@ -98,63 +69,5 @@ describe('S3Publisher', () => {
       })
       done()
     })
-
-    it('Uploads all files in a directory tree', async () => {
-      testPublisher.publish(testDir0, (err, data) => {
-        expect(data).to.be.an('object')
-        expect(data).to.have.property('ETag')
-        expect(data).to.have.property('s3file')
-      })
-
-      const objectList = await s3.listObjectsV2(
-        {
-          Bucket: process.env.AWS_S3_BUCKET,
-          Prefix: 'testS3Publisher'
-        }
-      ).promise()
-      expect(objectList).to.have.property('Contents')
-      expect(objectList).to.have.property('KeyCount')
-      expect(objectList.KeyCount).to.equal(objectList.Contents.length)
-      expect(objectList.KeyCount).to.be.gt(0)
-    }).timeout(10000)
-
-    it('Does not upload files that match defined exclusions', (done) => {
-      s3.getObject(
-        {
-          Bucket: process.env.AWS_S3_BUCKET,
-          Key: '/testS3Publisher/script.js.map'
-        }, (err, data) => {
-          expect(err).to.have.property('statusCode')
-          expect(err.statusCode).to.equal(404)
-      })
-
-      done()
-    }).timeout(10000)
-
-    it('Preserves the complete source tree when preserveSourceDir is specified', async () => {
-      let fullSourcePublisher = new S3Publisher({
-        keyPrefix: 'testS3Publisher',
-        bucket: process.env.AWS_S3_BUCKET,
-        preserveSourceDir: true
-      })
-
-      fullSourcePublisher.publish(testDir0, (err, data) => {
-        expect(data).to.be.an('object')
-        expect(data).to.have.property('ETag')
-        expect(data).to.have.property('s3file')
-      })
-
-      const objectList = await s3.listObjectsV2(
-        {
-          Bucket: process.env.AWS_S3_BUCKET,
-          Prefix: 'testS3Publisher/test'
-        }
-      ).promise()
-
-      expect(objectList).to.have.property('Contents')
-      expect(objectList).to.have.property('KeyCount')
-      expect(objectList.KeyCount).to.equal(objectList.Contents.length)
-      expect(objectList.KeyCount).to.be.gt(0)
-    }).timeout(10000)
   })
 })
